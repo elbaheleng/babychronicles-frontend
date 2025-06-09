5fd6fdd3d0c0d40731f00fad39061d9090fa0f70
elbaheleng <elbaheleng@gmail.com>
2025-06-09 09:44:57 +0530

    get element for About link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,10 @@ function Header() {
                 {user.email != 'admin@bc.com' && <div className='hidden md:flex justify-center items-center'>
                     <ul className='flex justify-center items-center gap-4'>
                         <Link to={'/'}><li>Home</li></Link>
-                        <a href="#about"><li>About</li></a>
+                        <a href="#about"onClick={(e) => {
+  e.preventDefault();
+  document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}}><li>About</li></a>
                         <a href='#testimonials'><li>Testimonials</li></a>
                     </ul>
                 </div>}
@@ -57,7 +60,10 @@ function Header() {
                 {user.email != 'admin@bc.com' && <div className='md:hidden flex justify-center items-center'>
                     <ul className='flex flex-col justify-center items-center gap-4'>
                         <Link to={'/'}><li>Home</li></Link>
-                        <a href="#about"><li>About</li></a>
+                        <a href="#about" onClick={(e) => {
+  e.preventDefault();
+  document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}}><li>About</li></a>
                         <a href='#testimonials'><li>Testimonials</li></a>
                     </ul>
                 </div>}
